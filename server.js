08cd81f60538eba3abb099717b29cc5536c978db
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-02-13 16:50:32 -0300

    Aumento do limite por pagina para 500

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.json());
 
 // Dev logging middleware
-if (process.env.NODE_ENV == 'development') {
+if (process.env.NODE_ENV == 'development ') {
   app.use(morgan('dev'));
 }
 
